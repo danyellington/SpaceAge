8a2128937e7d3a2626b00f7207dfc4ebf1800baf
Danyel Murray <danyelmurray1987@gmail.com>
2018-02-15 17:59:20 -0800

    add test to calculate age on venus and jupiter. pass.

--- spec/spaceAge-spec.js
+++ spec/spaceAge-spec.js
@@ -14,13 +14,23 @@ describe('spaceAge', function() {
   //   expect(secondsAge).toEqual("1");
   // });
 
-  // it('should calculate user age on Mercury', function() {
-  //   let userAge = new Age("12");
-  //   expect(userAge.ageMercury());
-  // })
+  it('should calculate user age on Mercury', function() {
+    let userAge = new Age("12");
+    expect(userAge.ageMercury()).toEqual(50);
+  })
 
   it('should calculate user age on Venus', function() {
     let userAge = new Age("12");
     expect(userAge.ageVenus()).toEqual(19);
+  })
+
+  it('should calculate user age on Mars', function() {
+    let userAge = new Age("45");
+    expect(userAge.ageMars()).toEqual(23);
+  })
+
+  it('should calculate user age on Jupiter', function() {
+    let userAge = new Age("30");
+    expect(userAge.ageJupiter()).toEqual(2);
   })
 });
